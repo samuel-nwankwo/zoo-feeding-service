ba41623bfee21c8603d8bdc1dc3f7c164f8f56f3
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-03 09:22:58 -0500

    Initial commit

---
+++ src/test/java/com/zoo/feedingevent/FeedingeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.feedingevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedingeventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
