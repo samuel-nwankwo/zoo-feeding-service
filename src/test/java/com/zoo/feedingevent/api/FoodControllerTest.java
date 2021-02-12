ee7f9b33ed49220dbdf4be297a30b3d989d2730f
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-12 09:56:56 -0500

    Injected MockMvc and FoodRepository in Food ControllerTest

--- src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
@@ -1,9 +1,19 @@
 package com.zoo.feedingevent.api;
 
+import com.zoo.feedingevent.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 
 @WebMvcTest
 public class FoodControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FoodRepository foodRepository;
 
 }
