65452c4c198011057c4082d2e97e86e73d0d2189
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-17 11:13:34 -0500

    Created testUpdateFood method

--- src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,4 +68,5 @@ public class FoodControllerTest {
                 .andExpect(jsonPath("$.id", Matchers.equalTo(2)))
                 .andExpect(jsonPath("$.name", Matchers.equalTo("hay")));
     }
+
 }
