57d02dd12a55747b269cdee1f809ba03daa07839
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-17 10:44:05 -0500

    Created testGetAllFoods method

--- src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
@@ -1,8 +1,16 @@
 package com.zoo.feedingevent.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.zoo.feedingevent.model.Food;
 import com.zoo.feedingevent.repository.FoodRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -22,10 +30,16 @@ public class FoodControllerTest {
     private FoodRepository foodRepository;
 
     @Test
-    public void testGetAllFoods(){
+    public void testGetAllFoods() throws Exception {
         List<Food> foodList = new ArrayList<>();
-        Food grass = new Food();
-        Food hay = new Food();
-//        foodList.add(grass);
+        Food grass = new Food(1L,"grass");
+        Food hay = new Food(2L,"hay");
+
+        foodList.add(grass);
+        foodList.add(hay);
+        Mockito.when(foodRepository.findAll()).thenReturn(foodList);
+        mockMvc.perform(get("/food")).andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(2)))
+                .andExpect(jsonPath("$[0].name", Matchers.equalTo("grass")));
     }
 }
