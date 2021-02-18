5b3bb232ba114c6a2ca759ecaefd362c3ddd8b6f
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-18 11:35:27 -0500

    Tested get event by Id method in event controller

--- src/test/java/com/zoo/feedingevent/api/EventControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/EventControllerTest.java
@@ -5,14 +5,18 @@ import com.zoo.feedingevent.model.Event;
 import com.zoo.feedingevent.repository.EventRepository;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,12 +35,23 @@ public class EventControllerTest {
     @Test
     public void testGetAllEvents() throws Exception {
         List<Event> eventList = new ArrayList<>();
-        Event event1 = new Event("Breakfast for the lions");
+        Event event1 = new Event("A new feeding event");
         eventList.add(event1);
         Mockito.when(eventRepository.findAll()).thenReturn(eventList);
         mockMvc.perform(get("/")).andExpect(status().isOk())
                 .andExpect(jsonPath("$", Matchers.hasSize(1)))
                 .andExpect(jsonPath("$[0].title", Matchers
-                .equalTo("Breakfast for the lions")));
+                .equalTo("A new feeding event")));
+    }
+    @Test
+    public void testGetEventById() throws Exception {
+         Event event2 = new Event("An old feeding event");
+
+        Mockito.when(eventRepository.findById(ArgumentMatchers.any()))
+                .thenReturn(Optional.of(event2));
+        mockMvc.perform((MockMvcRequestBuilders.get("/event/{id}", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", Matchers.equalTo("An old feeding event")));
     }
 }
