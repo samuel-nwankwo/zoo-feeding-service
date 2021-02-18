65e45c07b572c3ec11a9d4ce7ae49d4328ce0125
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-17 22:01:08 -0500

    Tested get animal by id method

--- src/test/java/com/zoo/feedingevent/api/AnimalControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/AnimalControllerTest.java
@@ -2,17 +2,22 @@ package com.zoo.feedingevent.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoo.feedingevent.model.Animal;
+import com.zoo.feedingevent.model.Food;
 import com.zoo.feedingevent.repository.AnimalRepository;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,12 +37,26 @@ public class AnimalControllerTest {
     @Test
     public void testGetAllAnimals() throws Exception {
         List<Animal> animalList = new ArrayList<>();
-        Animal bessy = new Animal("cow");
-        animalList.add(bessy);
+        Animal lion = new Animal("lion");
+        animalList.add(lion);
         Mockito.when(animalRepository.findAll()).thenReturn(animalList);
         mockMvc.perform(get("/animal")).andExpect(status().isOk())
                 .andExpect(jsonPath("$", Matchers.hasSize(1)))
-                .andExpect(jsonPath("$[0].breed", Matchers.equalTo("cow")));
+                .andExpect(jsonPath("$[0].breed", Matchers.equalTo("lion")));
 
+    }
+
+    @Test
+    public void testGetAnimalById() throws Exception {
+        Animal lion = new Animal(1L,"simba","lion");
+
+        Mockito.when(animalRepository.findById(ArgumentMatchers.any()))
+                .thenReturn(Optional.of(lion));
+        mockMvc.perform((MockMvcRequestBuilders.get("/animal/{id}", "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.name", Matchers.equalTo("simba")))
+                .andExpect(jsonPath("$.breed", Matchers.equalTo("lion")));
     }
 }
