ce12736d7ad8400c2e650fb0767aa0744567c55a
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-18 11:24:31 -0500

    tested get all events method for the controller class

--- src/test/java/com/zoo/feedingevent/api/EventControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/EventControllerTest.java
@@ -1,4 +1,42 @@
 package com.zoo.feedingevent.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zoo.feedingevent.model.Event;
+import com.zoo.feedingevent.repository.EventRepository;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(EventController.class)
 public class EventControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EventRepository eventRepository;
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    public void testGetAllEvents() throws Exception {
+        List<Event> eventList = new ArrayList<>();
+        Event event1 = new Event("Breakfast for the lions");
+        eventList.add(event1);
+        Mockito.when(eventRepository.findAll()).thenReturn(eventList);
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].title", Matchers
+                .equalTo("Breakfast for the lions")));
+    }
 }
