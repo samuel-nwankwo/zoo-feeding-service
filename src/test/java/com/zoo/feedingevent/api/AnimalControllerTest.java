07504853c03e62b458d4f82956f2f2962ec898e9
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-17 22:42:19 -0500

    Tested update method for animal controller

--- src/test/java/com/zoo/feedingevent/api/AnimalControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/AnimalControllerTest.java
@@ -2,7 +2,6 @@ package com.zoo.feedingevent.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zoo.feedingevent.model.Animal;
-import com.zoo.feedingevent.model.Food;
 import com.zoo.feedingevent.repository.AnimalRepository;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
@@ -12,13 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -66,15 +60,27 @@ public class AnimalControllerTest {
     }
     @Test
     public void testCreateAnimal() throws Exception {
-        Animal mufasa = new Animal(1L, "mufasa","lion");
-        Mockito.when(animalRepository.save(ArgumentMatchers.any())).thenReturn(mufasa);
-        String json = mapper.writeValueAsString(mufasa);
+        Animal lion = new Animal(1L, "mufasa","lion");
+        Mockito.when(animalRepository.save(ArgumentMatchers.any())).thenReturn(lion);
+        String json = mapper.writeValueAsString(lion);
         mockMvc.perform(post("/animal").contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("utf-8")
                 .content(json).accept(MediaType.APPLICATION_JSON)).andExpect(status()
                 .isCreated())
                 .andExpect(jsonPath("$.id", Matchers.equalTo(1)))
                 .andExpect(jsonPath("$.name", Matchers.equalTo("mufasa")))
+                .andExpect(jsonPath("$.breed", Matchers.equalTo("lion")));
+    }
+    @Test
+    public void testUpdateAnimal() throws Exception {
+        Animal lion = new Animal(2L, "nala", "lion");
+        Mockito.when(animalRepository.save(ArgumentMatchers.any())).thenReturn(lion);
+        String json = mapper.writeValueAsString(lion);
+        mockMvc.perform((MockMvcRequestBuilders.put("/animal/{id}","2")
+                .contentType(MediaType.APPLICATION_JSON).characterEncoding("utf-8")
+                .content(json).accept(MediaType.APPLICATION_JSON))).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.equalTo(2)))
+                .andExpect(jsonPath("$.name", Matchers.equalTo("nala")))
                 .andExpect(jsonPath("$.breed", Matchers.equalTo("lion")));
     }
 
