0a754d6a9d449cd330aaa4ef793d469f48f60426
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-17 21:15:31 -0500

    Created method for getting an animal by id

--- src/main/java/com/zoo/feedingevent/api/AnimalController.java
+++ src/main/java/com/zoo/feedingevent/api/AnimalController.java
@@ -1,16 +1,19 @@
 package com.zoo.feedingevent.api;
 
 import com.zoo.feedingevent.model.Animal;
+import com.zoo.feedingevent.model.Food;
 import com.zoo.feedingevent.repository.AnimalRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AnimalController {
@@ -28,5 +31,11 @@ public class AnimalController {
         animalRepository.findAll().forEach(animals::add);
         return new ResponseEntity<>(animals, HttpStatus.OK);
 
+    }
+    @GetMapping("/food/{id}")
+    public ResponseEntity<?> getAnimalById(@PathVariable Long id) {
+        Optional<Animal> animal = animalRepository.findById(id);
+        return animal.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
