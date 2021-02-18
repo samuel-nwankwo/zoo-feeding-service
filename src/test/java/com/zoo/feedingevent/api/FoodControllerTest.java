8bb3ff6e0f7227e2d3fdf2f7f6c34a95033dbf48
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-17 22:06:09 -0500

    Removed all unused imports from food controller test class

--- src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
+++ src/test/java/com/zoo/feedingevent/api/FoodControllerTest.java
@@ -1,16 +1,11 @@
 package com.zoo.feedingevent.api;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zoo.feedingevent.model.Animal;
 import com.zoo.feedingevent.model.Food;
 import com.zoo.feedingevent.repository.FoodRepository;
 import org.hamcrest.Matchers;
@@ -22,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
