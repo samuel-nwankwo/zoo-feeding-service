b5f4bd02a63e6e1b865b7d18810c43cde7faac03
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-19 08:57:06 -0500

    Updated initializer to create events on start up

--- src/main/java/com/zoo/feedingevent/Initializer.java
+++ src/main/java/com/zoo/feedingevent/Initializer.java
@@ -1,28 +1,77 @@
 package com.zoo.feedingevent;
 
+import com.zoo.feedingevent.model.Animal;
+import com.zoo.feedingevent.model.Event;
 import com.zoo.feedingevent.model.Food;
+import com.zoo.feedingevent.repository.AnimalRepository;
+import com.zoo.feedingevent.repository.EventRepository;
 import com.zoo.feedingevent.repository.FoodRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
+
 
 @Component
 public class Initializer implements CommandLineRunner {
 
-    private final FoodRepository repository;
+    private final FoodRepository foodRepository;
+    private final AnimalRepository animalRepository;
+    private final EventRepository eventRepository;
 
-    public Initializer(FoodRepository repository) {
-        this.repository = repository;
+    public Initializer(FoodRepository foodRepository, AnimalRepository animalRepository, EventRepository eventRepository) {
+        this.foodRepository = foodRepository;
+        this.animalRepository = animalRepository;
+        this.eventRepository = eventRepository;
     }
 
     @Override
     public void run(String... args) {
-        Stream.of("grass", "seeds", "hay",
-                "corn silage","grain").forEach(name ->
-                repository.save(new Food(name))
-        );
+        Stream.of(
+                new Food(1L,"grass"), new Food(2L,"hay"),
+                new Food(3L,"chicken"), new Food(4L, "steak")
+                ).forEach(foodRepository::save);
+        Set<Food> veggies = new HashSet<>();
+        Set<Food> meats = new HashSet<>();
+        veggies.add(foodRepository.findById(1L).orElse(null));
+        veggies.add(foodRepository.findById(2L).orElse(null));
+        meats.add(foodRepository.findById(3L).orElse(null));
+        meats.add(foodRepository.findById(4L).orElse(null));
 
-        repository.findAll().forEach(System.out::println);
+        Stream.of(
+                new Animal(1L,"cow1","cow"),
+                new Animal(2L,"cow2","cow"),
+                new Animal(3L, "lion1", "lion"),
+                new Animal(3L, "lion2", "lion")
+        ).forEach(animalRepository::save);
+        Set<Animal> cows = new HashSet<>();
+        Set<Animal> lions = new HashSet<>();
+        cows.add(animalRepository.findById(1L).orElse(null));
+        cows.add(animalRepository.findById(2L).orElse(null));
+        lions.add(animalRepository.findById(3L).orElse(null));
+        lions.add(animalRepository.findById(4L).orElse(null));
+
+        Event event1 = Event.builder()
+                .id(1L)
+                .date(Instant.now())
+                .title("Feeding the cows")
+                .description("Fed the cows breakfast")
+                .foods(veggies)
+                .animals(cows)
+                .build();
+        eventRepository.save(event1);
+
+        Event event2 = Event.builder()
+                .id(2L)
+                .date(Instant.now())
+                .title("Feeding the lions")
+                .description("Fed the lions break fast")
+                .foods(meats)
+                .animals(lions)
+                .build();
+        eventRepository.save(event1);
     }
+
 }
