fcc7f01da044d73c1f045ffd83178e012ecf6737
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-24 20:16:56 -0500

    Refactored repository interfaces

--- src/main/java/com/zoo/feedingevent/repository/FoodRepository.java
+++ src/main/java/com/zoo/feedingevent/repository/FoodRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FoodRepository extends CrudRepository<Food,Long> {
+public interface FoodRepository extends CrudRepository<Food,Long>{
 }
