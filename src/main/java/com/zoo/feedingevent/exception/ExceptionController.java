49d54daa7a5a656a127e987decbba28cba64e7e2
Samuel Nwankwo <nwankwosa@gmail.com>
2021-02-24 20:15:43 -0500

    Refactored exception controller

--- src/main/java/com/zoo/feedingevent/exception/ExceptionController.java
+++ src/main/java/com/zoo/feedingevent/exception/ExceptionController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-
 @ControllerAdvice
 public class ExceptionController extends ResponseEntityExceptionHandler {
 
